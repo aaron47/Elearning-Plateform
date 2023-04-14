01ca82f9b43a31ee90bd8ab4d11a750a250f9f2d
Aaron Borgi <aaronborgi1@gmail.com>
2023-04-14 19:43:18 +0100

    first commit: Basic authentication added

---
+++ app/Http/Controllers/AuthManager.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthManager extends Controller
+{
+    public static function login_post(Request $request)
+    {
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required|min:8"
+        ]);
+
+
+        $email = $request->input("email");
+        $password = $request->input("password");
+        $credentials = $request->only('email', 'password');
+
+
+        if (Auth::attempt($credentials)) {
+            return response("Login successful", 200);
+        }
+
+        return response("Wrong Credentials, please try again", 400);
+    }
+
+    public static function register_post(Request $request)
+    {
+        $request->validate([
+            "email" => "required|email|unique:users",
+            "password" => "required|min:8"
+        ]);
+
+        $data['email'] = $request->input("email");
+        $data['password'] = Hash::make($request->input("password"));
+        $user = User::create($data);
+
+        if (!$user) {
+            return response("Wrong Credentials, please try again", 400);
+        }
+
+        return response("Account created successfully", 201);
+    }
+}
